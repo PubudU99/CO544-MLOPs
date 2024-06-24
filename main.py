0e6daeb407476205e9e347228551b0eb085bd526
Pubudu Madusith <115542224+PubudU99@users.noreply.github.com>
2024-06-24 19:58:16 +0530

    Web UI done

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from mlProject import logger
+import warnings
+import os
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from mlProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
-
+from mlProject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -19,6 +21,11 @@ except Exception as e:
 
 
 STAGE_NAME = "Data Validation stage"
+
+warnings.filterwarnings("ignore", message="Setuptools is replacing distutils")
+os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/PubudU99/CO544-MLOPs.mlflow"
+os.environ["MLFLOW_TRACKING_USERNAME"]="PubudU99"
+os.environ["MLFLOW_TRACKING_PASSWORD"]="48c76b678398bd4ae08e042fb3a8bb9b0943292e"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataValidationTrainingPipeline()
@@ -53,3 +60,12 @@ except Exception as e:
         logger.exception(e)
         raise e
     
+STAGE_NAME = "Model evaluation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelEvaluationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
